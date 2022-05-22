a56ec710bfccbd9b602fbe4bd1465ae5e64356da
Prakharr18 <106022380+Prakharr18@users.noreply.github.com>
2022-05-22 19:54:41 +0530

    Add files via upload

---
+++ Thermometer/script.js
@@ -0,0 +1,37 @@
+
+const tempload = () => {
+
+    let temp = document.getElementById('temp');
+    temp.innerHTML = "&#xf2cb";
+
+
+    setTimeout(() =>  {
+
+        temp.innerHTML="&#xf2ca"; 
+        temp.style.color = '#fff';
+    } , 500);
+
+
+    setTimeout(() =>  {
+
+        temp.innerHTML="&#xf2c9"; 
+        temp.style.color = '#f8b627';
+    } , 2000);
+
+    setTimeout(() =>  {
+
+        temp.innerHTML="&#xf2c8;"; 
+    } , 3000);
+
+    setTimeout(() =>  {
+
+        temp.innerHTML="&#xf2c7"; 
+        temp.style.color = '#d63031';
+    } , 4000);
+}
+
+
+
+tempload();
+
+setInterval(tempload , 5000)
